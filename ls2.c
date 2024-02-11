6408907d9218107cf9de681fcfd37fe843e4c5d8
Spencer <soxe@pugetsound.edu>
2024-02-11 03:29:24 +0000

    comments

--- ls2.c
+++ ls2.c
@@ -54,13 +54,13 @@ void ls2(char* path, char* emp, int indent){
     }
     else if(emp != NULL){// MODE 2
         stack_t *list = initstack();
-        mode2(list, path, emp, indent);
+        mode2(list, path, emp, indent);//recursive fucntion for mode 2
         printstack(list);
-        freestack(list);
+        freestack(list);//give stack memory back
     }
 
 }
-
+//recursively goes through each directory in search of the given file of emp
 bool mode2(stack_t *list, char* path, char* emp, int indent){
     DIR* currDir = opendir(path);
     if(currDir == NULL){//load dir and check it
@@ -86,48 +86,48 @@ bool mode2(stack_t *list, char* path, char* emp, int indent){
             if(S_ISREG(statBuf.st_mode)){//its a file
                 if(strcmp(currPath->d_name,emp) == 0){
                     char intStr[30];
-                    snprintf(intStr, sizeof(intStr), " (%ld bytes)", statBuf.st_size);
+                    snprintf(intStr, sizeof(intStr), " (%ld bytes)", statBuf.st_size);//allocate space
                     char *currNameF = (char*)malloc((strlen(currPath->d_name) + (indent * strlen(INDENT)) + strlen(intStr) + 1) * sizeof(char));// 1 for null
-                    currNameF[0] = '\0';
+                    currNameF[0] = '\0';//null terminator
                     for(int i = 0; i < indent; i++){
                     strcat(currNameF, INDENT);
                     }
-                    strcat(currNameF, currPath->d_name);
+                    strcat(currNameF, currPath->d_name);//put file name together with indents and bytes
                    
                     strcat(currNameF, intStr);
                     push(list, currNameF);
                     
-                    foundFile = TRUE;
+                    foundFile = TRUE;//set the found file to true
                 }
             }
             
             else if(S_ISDIR(statBuf.st_mode)){//its a directory
             char *directory = " (directory)";
             char *currNameD = (char*)malloc((strlen(currPath->d_name) + (indent * strlen(INDENT)) + strlen(directory) + 1) * sizeof(char));// 1 for null
-                currNameD[0] = '\0';
+                currNameD[0] = '\0';//null terminator
                 foundFile = mode2(list, pathName, emp, indent+1);//was the file found down this path
-                bool childFound;
-                if(foundFile){
+                bool childFound;//boolean used to ensure another not found directory will change the foundfile variable
+                if(foundFile){//file found?
                     for(int i = 0; i < indent; i++){
                     strcat(currNameD, INDENT);
                     }
                     strcat(currNameD, currPath->d_name);
-                    strcat(currNameD, directory);
+                    strcat(currNameD, directory);//building directory name + directory
                     push(list, currNameD);
-                    childFound = TRUE;
+                    childFound = TRUE;//if the file is ever found down this path dont change it
                 }
                 else{
-                    free(currNameD);
+                    free(currNameD);//give mem back
                 }
                 if(childFound){
-                    foundFile = TRUE;
+                    foundFile = TRUE;//set it back to be correct
                 }
             }
-            free(pathName);
+            free(pathName);//give mem back
         }
     }
-    closedir(currDir);
-    return foundFile;
+    closedir(currDir);//close dir
+    return foundFile;//return if the file was found
 }
 
 
